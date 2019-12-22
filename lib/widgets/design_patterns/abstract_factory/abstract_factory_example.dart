8ad2dd0367ac3835289dceb01769f1d552206eca
mangirdaskazlauskas <kazlauskas@mangirdas.lt>
2019-12-22 14:02:53 +0200

    Initial commit

---
+++ lib/widgets/design_patterns/abstract_factory/abstract_factory_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AbstractFactoryExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Abstract Factory Example'),
+    );
+  }
+}
