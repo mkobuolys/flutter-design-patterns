7338054ad086f488644278cacaffddf51764939c
mangirdaskazlauskas <kazlauskas@mangirdas.lt>
2020-02-23 17:50:19 +0200

    Initial decorator design pattern commit.

---
+++ lib/widgets/design_patterns/decorator/decorator_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class DecoratorExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Decorator example!'),
+    );
+  }
+}
