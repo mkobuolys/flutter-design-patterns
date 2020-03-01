653aa0fff88eadd805dd90274941ba4b9432b20c
mangirdaskazlauskas <kazlauskas@mangirdas.lt>
2020-03-01 22:10:44 +0200

    Initial bridge design pattern commit.

---
+++ lib/widgets/design_patterns/bridge/bridge_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class BridgeExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Bridge Example!'),
+    );
+  }
+}
