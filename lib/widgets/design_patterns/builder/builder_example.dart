2694864b846561dd2e010e7960cce41d635b787f
mangirdaskazlauskas <kazlauskas@mangirdas.lt>
2020-04-05 19:31:59 +0300

    Initial builder design pattern commit. Refactoring code.

---
+++ lib/widgets/design_patterns/builder/builder_example.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class BuilderExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Builder example!'),
+      ),
+    );
+  }
+}
