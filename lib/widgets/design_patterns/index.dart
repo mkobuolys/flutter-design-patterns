b95c48840dc36a8813fb7fa32f98107477a7a481
mangirdaskazlauskas <kazlauskas@mangirdas.lt>
2020-10-27 23:12:01 +0200

    Initial commit for the visitor design pattern.

--- lib/widgets/design_patterns/index.dart
+++ lib/widgets/design_patterns/index.dart
@@ -18,3 +18,4 @@ export 'singleton/singleton_example.dart';
 export 'state/state_example.dart';
 export 'strategy/strategy_example.dart';
 export 'template_method/template_method_example.dart';
+export 'visitor/visitor_example.dart';
---
+++ lib/widgets/design_patterns/visitor/visitor_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class VisitorExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Visitor Example!'),
+    );
+  }
+}
