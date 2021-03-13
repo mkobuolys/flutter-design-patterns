b032237ff42be036c245e297d1508b3e1497b169
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2021-03-13 13:38:49 +0200

    Initial Mediator Design Pattern commit

---
+++ lib/widgets/design_patterns/mediator/mediator_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MediatorExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Mediator Example!'),
+    );
+  }
+}
