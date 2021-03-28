ad7cf2583cc6e0c7f9355d254ae20b80a37b401e
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2021-03-28 20:05:39 +0300

    Initial Observer Design Pattern commit

---
+++ lib/widgets/design_patterns/observer/observer_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ObserverExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Observer example'),
+    );
+  }
+}
